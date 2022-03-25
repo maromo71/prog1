68440ca2906c67b82ab6930aa84dd2c9b08564e7
maromo71 <professormoraes@gmail.com>
2022-03-24 22:23:18 -0300

    Aula do dia 24 de marco

--- aula2403/projetoFuncionario/src/com/company/GerenciarFuncionario.java
+++ aula2403/projetoFuncionario/src/com/company/GerenciarFuncionario.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import com.company.view.formFuncionario;
+
+import javax.swing.*;
+
 public class GerenciarFuncionario {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame tela = new JFrame();
+        tela.setContentPane(
+                new formFuncionario().getFuncionarioPanel());
+        tela.setSize(700, 500);
+        tela.setTitle("Cadastro de Funcionários");
+        tela.setDefaultCloseOperation(
+                WindowConstants.EXIT_ON_CLOSE);
+        tela.setVisible(true);
     }
 
 
--- aula2403/projetoFuncionario/src/com/company/view/formFuncionario.java
+++ aula2403/projetoFuncionario/src/com/company/view/formFuncionario.java
@@ -3,6 +3,8 @@ package com.company.view;
 import com.company.Funcionario;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +22,86 @@ public class formFuncionario {
     private JButton btnSalvar;
     private List<Funcionario> listaFuncionario =
             new ArrayList<>();
+
+    public formFuncionario() {
+        btnProcurar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(listaFuncionario.size()>0){
+                    int proc = Integer.parseInt(txtMatricula.getText());
+                    Funcionario f = buscarFuncionarioNaLista(proc);
+                    txtMatricula.setText(Integer.toString(proc));
+                    txtNome.setText(f.getNome());
+                    txtEmail.setText(f.getEmail());
+                    txtSetor.setText(f.getSetor());
+                    txtSalario.setText(Double.toString(f.getSalario()));
+                }
+            }
+        });
+        btnSalvar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int proc = Integer.parseInt(txtMatricula.getText());
+                Funcionario f = buscarFuncionarioNaLista(proc);
+                if(f==null){
+                    f = new Funcionario();
+                    manterFuncionario(f);
+                    listaFuncionario.add(f);
+                    JOptionPane.showMessageDialog(null,
+                            "Cadastrado com sucesso",
+                            "Funcionario",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }else{
+                    manterFuncionario(f);
+                    JOptionPane.showMessageDialog(null,
+                            "Funcionário alterado com sucesso",
+                            "Funcionario",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+                limparFormulario();
+            }
+        });
+        btnListar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("Dados dos Funcionários: \n\n");
+                for (Funcionario f : listaFuncionario){
+                    sb.append(f.toString());
+                }
+                JOptionPane.showMessageDialog(null,
+                        sb.toString(),
+                        "Funcionários",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+
+    private void manterFuncionario(Funcionario f) {
+        f.setMatricula(Integer.parseInt(txtMatricula.getText()));
+        f.setNome(txtNome.getText());
+        f.setEmail(txtEmail.getText());
+        f.setSetor(txtSetor.getText());
+        f.setSalario(Double.parseDouble(txtSalario.getText()));
+    }
+
+    private Funcionario buscarFuncionarioNaLista(int proc) {
+        for(Funcionario f : listaFuncionario){
+            if(f.getMatricula()==proc){
+                return f;
+            }
+        }
+        return null;
+    }
+
+    private void limparFormulario() {
+        txtMatricula.setText("");
+        txtNome.setText("");
+        txtSetor.setText("");
+        txtSalario.setText("");
+        txtEmail.setText("");
+        txtMatricula.requestFocus();
+    }
 
     public JPanel getFuncionarioPanel() {
         return funcionarioPanel;
